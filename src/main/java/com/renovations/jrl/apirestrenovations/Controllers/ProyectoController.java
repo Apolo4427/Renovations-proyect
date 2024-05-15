226460ff65cfd0a5cf47e19ba9bc4ac1bdda613c
apolo4427 <top152695@gmail.com>
2024-05-14 22:16:14 -0500

    reparando controlador de proyectos, @RestController

--- src/main/java/com/renovations/jrl/apirestrenovations/Controllers/ProyectoController.java
+++ src/main/java/com/renovations/jrl/apirestrenovations/Controllers/ProyectoController.java
@@ -5,10 +5,12 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.renovations.jrl.apirestrenovations.Entities.Proyecto;
 import com.renovations.jrl.apirestrenovations.Services.ProyectoServices.ProyectoServicesImp;
 
+@RestController
 public class ProyectoController {
 
     @Autowired 
