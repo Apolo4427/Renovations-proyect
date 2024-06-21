846108fdc1cff89e80acb8c3494deba0a2f39d0a
apolo4427 <top152695@gmail.com>
2024-06-20 20:14:04 -0500

    corrigiendo error al querer editar un cliente al cual no se le desea editar el correo

--- src/main/java/com/renovations/jrl/apirestrenovations/Controllers/ClienteController.java
+++ src/main/java/com/renovations/jrl/apirestrenovations/Controllers/ClienteController.java
@@ -3,13 +3,13 @@ package com.renovations.jrl.apirestrenovations.Controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -67,8 +67,19 @@ public class ClienteController {
 
     //METODO PUT
     @PatchMapping("/actualizar/{id}")
-    public Cliente updateCliente(@PathVariable Long id, @RequestBody Cliente cliente) throws ClienteNoFundException{
-        return serviceClienteImpl.actualizarCliente(id, cliente);
+    public ResponseEntity<Cliente> updateCliente(@PathVariable Long id, @RequestBody Cliente cliente) throws ClienteNoFundException{
+
+        Cliente clienteActualizado;
+
+        if(cliente.getEmail() == null){
+            Cliente clienteBuscado = findClientePorID(id);
+            cliente.setEmail(clienteBuscado.getEmail());
+            clienteActualizado = serviceClienteImpl.actualizarCliente(id, cliente);
+        }else{
+            clienteActualizado = serviceClienteImpl.actualizarCliente(id, cliente);
+        }
+        
+        return ResponseEntity.ok().body(clienteActualizado);
     }
     
     //METODO DELETE
